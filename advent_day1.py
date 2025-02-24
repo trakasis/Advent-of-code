555803583df71fadd565784020893d1aee56b84e
Joshua Demeter <joshua.demeter@adtran.com>
2025-02-24 17:01:59 -0600

    some steps

--- advent_day1.py
+++ advent_day1.py
@@ -1,9 +1,14 @@
 # Advent of Code Day 1
 import os
 import sys
+import numpy
+import math
 
 def main():
-    print("Hello, World!")
-
+    data = numpy.loadtxt("input.txt")
+    first_row = data[:,0]
+    second_row = data[:,1]
+    print(abs(first_row[0] - second_row[0]))
+    
 if __name__ == "__main__":
     main()
